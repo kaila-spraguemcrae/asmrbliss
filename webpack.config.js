05859c2c2379f16782c2282b5cec8e939893e3b4
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-04 09:30:00 -0800

    update dev environment to fix local repos

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/asmrbliss/"
   },
   devtool: "eval-source-map",
   devServer: {
